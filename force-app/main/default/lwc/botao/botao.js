c1f671f1338c5fa66c5606bd219d427877fb852c
Antonio-Mako <71250901+Antonio-Mako@users.noreply.github.com>
2022-04-26 13:53:57 -0300

    alterações de layout do modal

--- force-app/main/default/lwc/botao/botao.js
+++ force-app/main/default/lwc/botao/botao.js
@@ -25,4 +25,28 @@ export default class Botao extends LightningElement {
         this.dispatchEvent(event);
     }
 
+    handleMaskCpf(){
+        document.getElementById('cnpj').addEventListener('input', function(e) {
+            var x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,3})(\d{0,3})(\d{0,4})(\d{0,2})/);
+            e.target.value = !x[2] ? x[1] : x[1] + '.' + x[2] + '.' + x[3] + '/' + x[4] + (x[5] ? '-' + x[5] : '');
+          });
+    }
+
+    formatarCampo(campoTexto) {
+        if (campoTexto.value.length <= 11) {
+            campoTexto.value = mascaraCpf(campoTexto.value);
+        } else {
+            campoTexto.value = mascaraCnpj(campoTexto.value);
+        }
+    }
+     retirarFormatacao(campoTexto) {
+        campoTexto.value = campoTexto.value.replace(/(\.|\/|\-)/g,"");
+    }
+    mascaraCpf(valor) {
+        return valor.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g,"\$1.\$2.\$3\-\$4");
+    }
+    mascaraCnpj(valor) {
+        return valor.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/g,"\$1.\$2.\$3\/\$4\-\$5");
+    }
+
 }
